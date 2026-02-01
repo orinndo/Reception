7026c273ea8297f7ca31f17829546ef82a7659d3
orinndo <Armbanduhr1945@gmail.com>
2026-02-01 22:37:00 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -547,6 +547,7 @@
 
   function showPatient(mode) {
     patientMode = mode;
+    document.body.dataset.lastPatientMode = mode;
 
     // Switch view first so the button always "does something"
     staffView.classList.add('is-hidden');
